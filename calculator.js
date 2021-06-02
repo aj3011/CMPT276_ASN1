8b6c3e0c4e9b1d46bbe7f011f6280cffb40c7012
aj3011 <ashreyjairath@gmail.com>
2021-06-02 15:14:07 -0700

    Improved javscript interactivity

--- calculator.js
+++ calculator.js
@@ -1,54 +1,117 @@
-var w1 = Number(document.getElementById("w1").value);
-var w2 = Number(document.getElementById("w2").value);
-var w3 = Number(document.getElementById("w3").value);
-var w4 = Number(document.getElementById("w4").value);
-
-var a1f1 = Number(document.getElementById("a1f1").value);
-var a1f2 = Number(document.getElementById("a1f2").value);
-var a2f1 = Number(document.getElementById("a2f1").value);
-var a2f2 = Number(document.getElementById("a2f2").value);
-var a3f1 = Number(document.getElementById("a3f1").value);
-var a3f2 = Number(document.getElementById("a3f2").value);
-var a4f1 = Number(document.getElementById("a4f1").value);
-var a4f2 = Number(document.getElementById("a4f2").value);
+function getValue(id) {
+  var value = parseFloat(
+    document.getElementById(id).value.length == 0
+      ? "0"
+      : document.getElementById(id).value
+  );
+  console.log(typeof value);
+  return value;
+}
 
 function calculateWeight() {
-  console.log(w3);
+  // var w1 = getValuegetElementById("w1")length == 0
+  //     ? "0"
+  //     : document.getElementById("w1")// );
+  var w1 = getValue("w1");
+  console.log("sex" + typeof w1);
+
+  var w2 = getValue("w2");
+  var w3 = getValue("w3");
+
+  var w4 = getValue("w4");
+  console.log(w4);
+  var a1f1 = getValue("a1f1");
+  var a1f2 = getValue("a1f2");
+  var a2f1 = getValue("a2f1");
+  var a2f2 = getValue("a2f2");
+  var a3f1 = getValue("a3f1");
+  var a3f2 = getValue("a3f2");
+  var a4f1 = getValue("a4f1");
+  var a4f2 = getValue("a4f2");
+
+  document.getElementById("first").innerHTML = "-";
+  document.getElementById("second").innerHTML = "-";
+  document.getElementById("third").innerHTML = "-";
+  document.getElementById("fourth").innerHTML = "-";
+
+  let numerator = 0;
+  let denominator = 0;
   console.log(a2f2);
-  var weight = Number(
-    (w1 * (a1f1 / a1f2) +
-      w2 * (a2f1 / a2f2) +
-      w3 * (a3f1 / a3f2) +
-      w4 * (a4f1 / a4f2)) /
-      (w1 + w2 + w3 + w4)
-  );
-  console.log(weight);
+  if (w1 != 0 && a1f2 != 0) {
+    numerator += w1 * (a1f1 / a1f2);
+    document.getElementById("first").innerHTML = a1f1 / a1f2;
+    denominator += w1;
+  }
+
+  if (w2 != 0 && a2f2 != 0) {
+    numerator += w2 * (a2f1 / a2f2);
+    document.getElementById("second").innerHTML = a2f1 / a2f2;
+    denominator += w2;
+  }
+
+  if (w3 != 0 && a3f2 != 0) {
+    numerator += w3 * (a3f1 / a3f2);
+    document.getElementById("third").innerHTML = a3f1 / a3f2;
+    denominator += w3;
+  }
+
+  if (w4 != 0 && a4f2 != 0) {
+    numerator += w4 * (a4f1 / a4f2);
+    document.getElementById("fourth").innerHTML = a4f1 / a4f2;
+    denominator += w4;
+  }
+
+  let result = numerator / denominator;
+  document.getElementById("result").innerHTML = "Result " + result;
 }
 
 function calculateMean() {
-  var num = 0;
-  var sum = 0;
+  var w1 = getValue("w1");
+  console.log(w1);
 
-  if (a1f1 != null && a1f2 != null) {
-    sum += a1f1 / a2f2;
+  var w4 = getValue("w4");
+  console.log(w4);
+  var a1f1 = getValue("a1f1");
+  var a1f2 = getValue("a1f2");
+  var a2f1 = getValue("a2f1");
+  var a2f2 = getValue("a2f2");
+  var a3f1 = getValue("a3f1");
+  var a3f2 = getValue("a3f2");
+  var a4f1 = getValue("a4f1");
+  var a4f2 = getValue("a4f2");
+  var num = 0.0;
+  var sum = 0.0;
+
+  document.getElementById("first").innerHTML = "-";
+  document.getElementById("second").innerHTML = "-";
+  document.getElementById("third").innerHTML = "-";
+  document.getElementById("fourth").innerHTML = "-";
+  console.log(w4);
+  if (a1f2 != 0) {
+    sum += a1f1 / a1f2;
+    document.getElementById("first").innerHTML = a1f1 / a1f2;
     num += 1;
   }
 
-  if (a2f1 != null && a2f2 != null) {
+  if (a2f2 != 0) {
     sum += a2f1 / a2f2;
+    document.getElementById("second").innerHTML = a2f1 / a2f2;
     num += 1;
   }
 
-  if (a3f1 != null || a3f2 != null) {
+  if (a3f2 != 0) {
     sum += a3f1 / a3f2;
+    document.getElementById("third").innerHTML = a3f1 / a3f2;
     num += 1;
   }
 
-  if (a4f1 != null || a4f2 != null) {
+  if (a4f2 != 0) {
     sum += a4f1 / a4f2;
+    document.getElementById("fourth").innerHTML = a4f1 / a4f2;
     num += 1;
   }
-
+  console.log(sum + "******" + num);
   var mean = sum / num;
+  document.getElementById("result").innerHTML = "Result " + mean;
   console.log(mean);
 }
